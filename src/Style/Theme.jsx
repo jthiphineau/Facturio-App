77079ec8133b238a60e5023732f09c8d1b360c91
Julien Thiphineau <j.thiphineau@yahoo.fr>
2023-03-30 13:48:20 +0200

    last update

---
+++ src/Style/Theme.jsx
@@ -0,0 +1,16 @@
+/**
+ * Contient toutes les variables css
+ */
+export const Theme = {
+  colors: {
+    white: '#edfffb',
+    black: '#2D2B2B',
+    green: '#557859',
+    blue: '#264f4a',
+    red: '#880909',
+  },
+  fonts: {
+    display: `'Poppins', sans-serif`,
+    normal: `'Poppins', sans-serif`,
+  },
+}
